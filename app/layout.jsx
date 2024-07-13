719339c7d193c1c6c3c2019a3a69897624c47841
Rodrigo Pereira <rodrigogr.pereira@gmail.com>
2024-07-12 21:34:09 -0300

    themeToggler

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,6 +1,12 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 
+//components
+import Header from "@/components/Header";
+import Footer from "@/components/Footer";
+// theme provider
+import { ThemeProvider } from "@/components/ThemeProvider";
+
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
@@ -11,7 +17,13 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+      <ThemeProvider attribute="class" defaultTheme='light'>
+        <Header />
+        {children}
+        <Footer />
+        </ThemeProvider>
+        </body>
     </html>
   );
 }
